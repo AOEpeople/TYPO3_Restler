28d8a752a2e89d35b76cbc8ae6b43fc0ce018585
juergen.kussmann <juergen.kussmann@aoe.com>
2015-03-26 07:03:43 +0100

    add simple TYPO3-extension configuration

---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'restler',
+    'description' => 'restler for TYPO3',
+    'category' => 'plugin',
+    'author' => 'AOE GmbH',
+    'author_company' => 'AOE GmbH',
+    'author_email' => 'dev@aoe.com',
+    'shy' => 0,
+    'dependencies' => 'extbase',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => 'typo3temp/tx_restler',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-0.0.0',
+            'php' => '5.3.0-0.0.0',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => '',
+);
