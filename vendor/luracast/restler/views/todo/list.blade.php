29144fc095118bb8241a6b0ab2886d1fad6fa160
kevin.schu <kevin.schu@aoe.com>
2015-07-24 12:11:34 +0200

    delete vendor

--- vendor/luracast/restler/views/todo/list.blade.php
+++
@@ -1,9 +0,0 @@
-        <li id="todo-{{ $id }}" class="todo">
-            <div class="text">{{ $text }}</div>
-
-            <div class="actions">
-                <a href="#" class="edit">Edit</a>
-                <a href="#" class="delete">Delete</a>
-            </div>
-
-        </li>
