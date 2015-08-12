49f55aaec338c2e0f37a3cb12a1ef35e7d0c4d74
kevin.schu <kevin.schu@aoe.com>
2015-08-12 14:43:09 +0200

    standalone unit tests

--- Tests/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase_reflection']['backend'] = 't3lib_cache_backend_NullBackend';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['extbase_object']['backend'] = 't3lib_cache_backend_NullBackend';
