c56479e897fb3edf9773492fed19ad54b4b34207
Technical user for Jenkins <jenkins@congstar-congo.itg.de2.dc.aoe.lan>
2015-08-17 10:55:00 +0200

    update version in ext_emconf.php to 0.9.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,41 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-    'title' => 'restler',
-    'description' => 'restler for TYPO3',
-    'category' => 'be',
-    'author' => 'AOE GmbH',
-    'author_company' => 'AOE GmbH',
-    'author_email' => 'dev@aoe.com',
-    'shy' => 0,
-    'dependencies' => 'extbase',
-    'conflicts' => '',
-    'priority' => '',
-    'module' => '',
-    'state' => 'beta',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => 'typo3temp/tx_restler',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
-    'version' => '0.8.0',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '6.2.0-6.2.99',
-            'php' => '5.3.4',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-    '_md5_values_when_last_written' => '',
+/***************************************************************
+ * Extension Manager/Repository config file for ext "restler".
+ *
+ * Auto generated 17-08-2015 10:54
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'restler',
+	'description' => 'restler for TYPO3',
+	'category' => 'be',
+	'author' => 'AOE GmbH',
+	'author_company' => 'AOE GmbH',
+	'author_email' => 'dev@aoe.com',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => 'typo3temp/tx_restler',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.9.0',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.0-6.2.99',
+			'php' => '5.3.4',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'_md5_values_when_last_written' => '',
 );
+
