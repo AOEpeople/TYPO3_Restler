543586610a98f8441e03dacf8b39e6a15a7da477
soda-2005 <jkussmann@web.de>
2015-11-05 14:55:58 +0100

    [REFACTORING] refactored class RestApiClient (for a better overview)

--- Classes/System/Restler/RestApiClient.php
+++ Classes/System/Restler/RestApiClient.php
@@ -75,29 +75,48 @@ class RestApiClient implements SingletonInterface
     public function executeRequest($requestMethod, $requestUri, array $getData = array(), array $postData = array())
     {
         if ($this->isRequestPreparationRequired()) {
-            $this->prepareRestApiRequest();
+            $this->prepareRequest();
         }
 
         try {
             $this->isExecutingRequest = true;
-            $restApiRequest = new RestApiRequest($this->restApiRequestScope);
-            $result = $restApiRequest->executeRestApiRequest($requestMethod, $requestUri, $getData, $postData);
+            $result = $this->createRequest()->executeRestApiRequest($requestMethod, $requestUri, $getData, $postData);
             $this->isExecutingRequest = false;
-
             return $result;
         } catch(RestException $e) {
             $this->isExecutingRequest = false;
-
-            $errorMessage = 'internal REST-API-request \''.$requestMethod.':'.$requestUri.'\' could not be processed';
-            if (false === $this->restApiRequestScope->getOriginalRestApiRequest()->getProductionMode()) {
-                $errorMessage .= ' (message: '.$e->getMessage().', details: '.json_encode($e->getDetails()).')';
-            }
-            throw new RestApiRequestException(
-                $errorMessage,
-                RestApiRequestException::EXCEPTION_CODE_REQUEST_COULD_NOT_PROCESSED,
-                $e
-            );
+            $e = $this->createRequestException($e, $requestMethod, $requestUri);
+            throw $e;
         }
+    }
+
+    /**
+     * We must create for every REST-API-request a new object, because the object will contain data, which is related to the request
+     *
+     * @return RestApiRequest
+     */
+    protected function createRequest()
+    {
+        return new RestApiRequest($this->restApiRequestScope);
+    }
+
+    /**
+     * @param RestException $e
+     * @param string        $requestMethod
+     * @param string        $requestUri
+     * @return RestApiRequestException
+     */
+    protected function createRequestException(RestException $e, $requestMethod, $requestUri)
+    {
+        $errorMessage = 'internal REST-API-request \''.$requestMethod.':'.$requestUri.'\' could not be processed';
+        if (false === $this->restApiRequestScope->getOriginalRestApiRequest()->getProductionMode()) {
+            $errorMessage .= ' (message: '.$e->getMessage().', details: '.json_encode($e->getDetails()).')';
+        }
+        return new RestApiRequestException(
+            $errorMessage,
+            RestApiRequestException::EXCEPTION_CODE_REQUEST_COULD_NOT_PROCESSED,
+            $e
+        );
     }
 
     /**
@@ -128,7 +147,7 @@ class RestApiClient implements SingletonInterface
      * build the 'original' REST-API-Request (aka Restler-object, which is always
      * required) and store it in the REST-API-Request-Scope (aka Scope-object)
      */
-    private function prepareRestApiRequest()
+    private function prepareRequest()
     {
         $originalRestApiRequest = $this->getRestlerBuilder()->build();
         $this->restApiRequestScope->storeOriginalRestApiRequest($originalRestApiRequest);
