b2f2b91be7ebdd3e20b3270454611b771afdd8a3
juergen.kussmann <juergen.kussmann@aoe.com>
2016-01-20 08:35:12 +0100

    [BUGFIX] fix version (version of composer.json and ext_emconf.php should be equal!)

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => 0,
 	'createDirs' => 'typo3temp/tx_restler',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.2.2',
+	'version' => '1.2.3',
 	'constraints' => 
 	array (
 		'depends' => 
