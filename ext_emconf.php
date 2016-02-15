9889bf2c68c1356f28f5d926a3add9e2be343dfa
juergen.kussmann <juergen.kussmann@aoe.com>
2016-02-15 07:54:48 +0100

    [BUGFIX] remove version from composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => 0,
 	'createDirs' => 'typo3temp/tx_restler',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.2.12',
+	'version' => '1.2.13',
 	'constraints' => 
 	array (
 		'depends' => 
