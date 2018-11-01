1046130a72f804b0d932f7d05e68f96d68299ab8
Technical user for Jenkins <jenkins@congstar-congo.itg.de2.dc.aoe.lan>
2018-11-01 03:19:45 +0100

    [TASK] Updating to version 2.1.0 [ci skip]

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,40 @@
 <?php
+
 /***************************************************************
  * Extension Manager/Repository config file for ext "restler".
+ *
+ * Auto generated 01-11-2018 03:19
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-    'title' => 'restler',
-    'description' => 'This extension integrates the REST-API-framework "restler" in TYPO3',
-    'category' => 'fe',
-    'author' => 'AOE GmbH',
-    'author_company' => 'AOE GmbH',
-    'author_email' => 'dev@aoe.com',
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'clearCacheOnLoad' => 0,
-    'version' => '2.0.4',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '7.6.0-8.7.99',
-            'php' => '5.5.0',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'restler',
+  'description' => 'This extension integrates the REST-API-framework "restler" in TYPO3',
+  'category' => 'fe',
+  'author' => 'AOE GmbH',
+  'author_company' => 'AOE GmbH',
+  'author_email' => 'dev@aoe.com',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'clearCacheOnLoad' => 0,
+  'version' => '2.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.7.99',
+      'php' => '5.5.0',
     ),
-    '_md5_values_when_last_written' => '',
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => '',
 );
+
