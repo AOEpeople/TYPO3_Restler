7ea0ac97b139930e4da8a4c4b490c7e08dee2430
Stefan Masztalerz <stefan.masztalerz@aoe.com>
2019-03-26 14:14:03 +0100

    [ADD] middleware concept requires typo3 9.2+

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
   'version' => '3.1.0',
   'constraints' => [
     'depends' => [
-      'typo3' => '8.7.0-9.5.99'
+      'typo3' => '9.5.0-9.5.99'
     ],
     'conflicts' => [],
     'suggests' => [],
