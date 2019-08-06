65b5a10994cb382edc14f7a1e51a655ee51883d4
Sven Carstens <sven.carstens@aoe.com>
2019-08-06 15:11:44 +0200

    [Fix] Handle Deprecation: #82254, TYPO_CONF_VARS.EXT.extConf is deprecated

--- Classes/Configuration/ExtensionConfiguration.php
+++ Classes/Configuration/ExtensionConfiguration.php
@@ -26,6 +26,7 @@ namespace Aoe\Restler\Configuration;
  ***************************************************************/
 
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * @package Restler
@@ -43,7 +44,7 @@ class ExtensionConfiguration implements SingletonInterface
      */
     public function __construct()
     {
-        $this->configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['restler']);
+        $this->configuration = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('restler');
     }
 
     /**
