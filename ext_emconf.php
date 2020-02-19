ab1bfb30f684bc2fbe33c7c82457b508bf875dfd
Christian Krzeminski <christian.krzeminski@aoe.com>
2020-02-19 13:26:14 +0100

    [TASK] update supported PHP version

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.1',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.2.99',
+            'php' => '7.2.0-7.4.99',
             'typo3' => '8.7.0-8.7.99'
         ],
         'conflicts' => [],
