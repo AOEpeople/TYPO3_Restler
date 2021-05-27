f06d03ab92f1cd180ad5119eb2aa1d17217a46f4
Felix Semmler <felix.semmler@aoe.com>
2021-05-27 22:20:16 +0200

    [TASK] Cleanup and adapt branch for TYPO3V9

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
   'version' => '9.0.0',
   'constraints' => [
     'depends' => [
-      'typo3' => '9.5.0-10.4.99'
+      'typo3' => '9.5.0-9.5.99'
     ],
     'conflicts' => [],
     'suggests' => [],
