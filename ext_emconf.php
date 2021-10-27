93d231ac685d89c27061b3a686f3bc5c96f7664f
Thomas Schenk <Thomas.Schenk@aoe.com>
2021-10-27 10:18:22 +0200

    [RELEASE] Restler version 10.1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,10 +20,10 @@ $EM_CONF[$_EXTKEY] = [
   'state' => 'stable',
   'uploadfolder' => 0,
   'clearCacheOnLoad' => 0,
-  'version' => '10.1.1',
+  'version' => '10.1.2',
   'constraints' => [
     'depends' => [
-      'typo3' => '10.4.0-10.4.99'
+      'typo3' => '10.4.0-11.5.99'
     ],
     'conflicts' => [],
     'suggests' => [],
