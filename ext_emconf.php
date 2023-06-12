3905ed93c1d7c1044d608302f329db2194a74e39
thomas.layh <thomas.layh@aoe.com>
2023-06-12 09:47:57 +0200

    [RELEASE] fix ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF['restler'] = [
   'version' => '11.0.4',
   'constraints' => [
     'depends' => [
-      'typo3' => '11.0-11.5.99'
+      'typo3' => '11.0.0-11.5.99'
     ],
     'conflicts' => [],
     'suggests' => [],
