5a7f7aec5faaa318cbf35497d07ea71122d4a33c
Christian Krzeminski <christian.krzeminski@aoe.com>
2024-07-03 14:08:43 +0200

    chore: ecs config

--- .code-quality/ecs.php
+++ .code-quality/ecs.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\FunctionNotation\FunctionTypehintSpaceFixer;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
 use PhpCsFixer\Fixer\Phpdoc\GeneralPhpdocAnnotationRemoveFixer;
-use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayListItemNewlineFixer;
 use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
-use Symplify\CodingStandard\Fixer\LineLength\DocBlockLineLengthFixer;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
@@ -36,17 +33,9 @@ return ECSConfig::configure()
     )
     ->withSkip([
         NotOperatorWithSuccessorSpaceFixer::class => null,
-        DocBlockLineLengthFixer::class => null,
         ArrayListItemNewlineFixer::class => null,
         ArrayOpenerAndCloserNewlineFixer::class => null,
-        FunctionTypehintSpaceFixer::class => [
-            __DIR__ . '/../Tests/Unit/TYPO3/AdditionalResponseHeadersTest.php',
-            __DIR__ . '/../Classes/TYPO3/Hooks/ClearCacheMenuHook.php',
-            __DIR__ . '/../Classes/TYPO3/Configuration/ExtensionConfiguration.php',
-        ],
-        DeclareStrictTypesFixer::class => null,
         GeneralPhpdocAnnotationRemoveFixer::class => null,
         RenameParamToMatchTypeRector::class => null,
-
     ])
     ->withSpacing(OPTION::INDENTATION_SPACES, "\n");
